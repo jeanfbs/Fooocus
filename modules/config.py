964ee7fda28b43960fc0ec240cc637f383691f2f
Jean Fabricio Batista Santos     (DEVIRES) <jeansantos.devires@latam.com>
2025-06-06 12:23:34 -0300

    improvements

--- modules/config.py
+++ modules/config.py
@@ -291,13 +291,13 @@ default_refiner_switch = get_config_item_or_set_default(
 )
 default_loras_min_weight = get_config_item_or_set_default(
     key='default_loras_min_weight',
-    default_value=-2,
+    default_value=-4,
     validator=lambda x: isinstance(x, numbers.Number) and -10 <= x <= 10,
     expected_type=numbers.Number
 )
 default_loras_max_weight = get_config_item_or_set_default(
     key='default_loras_max_weight',
-    default_value=2,
+    default_value=4,
     validator=lambda x: isinstance(x, numbers.Number) and -10 <= x <= 10,
     expected_type=numbers.Number
 )
